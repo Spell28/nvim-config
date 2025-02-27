d8a3e5f0d0ee14a42c9d74e67a34d61de8569e46
Spell28 <bacujlek@gmail.com>
2025-02-28 02:32:26 +0600

    feat: scroll smooth plugin and mapping g and GG

--- lua/mappings.lua
+++ lua/mappings.lua
@@ -5,17 +5,34 @@ local nomap = vim.keymap.del
 
 map("n", ";", ":", { desc = "CMD enter command mode" })
 map("i", "jj", "<ESC>")
-map("n", "<leader>s", ":write! <CR>", {desc = "Save current file"})
-map("n", "<leader>q", ":qa! <CR>", {desc = "Quite from nvim"})
-map("n", "<leader>fl", ":OrganizeImports <CR>", {desc = "Organize imports"})
-map("n", "<leader>tt", ":tabnew <CR>", {desc = "New tab"})
-map("n", "<leader>tx", ":tabclose <CR>", {desc = "Close current tab"})
-map("n", "<Tab>", ":tabnext <CR>", {desc = "Next tab"})
-map("n", "<S-Tab>", ":tabprev <CR>", {desc = "Previous tab"})
-map("v", ">", ">gv", {desc = "Indent right"})
-map("v", "<", "<gv", {desc = "Indent left"})
+map("n", "<leader>s", ":write! <CR>", { desc = "Save current file" })
+map("n", "<leader>q", ":qa! <CR>", { desc = "Quite from nvim" })
+map("n", "<leader>fl", ":OrganizeImports <CR>", { desc = "Organize imports" })
+map("n", "<leader>tt", ":tabnew <CR>", { desc = "New tab" })
+map("n", "<leader>tx", ":tabclose <CR>", { desc = "Close current tab" })
+map("n", "<Tab>", ":tabnext <CR>", { desc = "Next tab" })
+map("n", "<S-Tab>", ":tabprev <CR>", { desc = "Previous tab" })
+map("v", ">", ">gv", { desc = "Indent right" })
+map("v", "<", "<gv", { desc = "Indent left" })
 map("v", "J", ":m '>+1<CR>gv=gv", { desc = "Move selected text down" })
 map("v", "K", ":m '<-2<CR>gv=gv", { desc = "Move selected text up" })
+
+-- Прокрутка на начало файла с динамическим расчетом
+map(
+  "n",
+  "gg",
+  ':lua require("neoscroll").scroll(-2 * vim.api.nvim_buf_line_count(0), { move_cursor = true, duration = 0.5, easing = quadratic, info = { kind = "gg", winid = vim.api.nvim_get_current_win(), bufnr = vim.api.nvim_get_current_buf()} })<CR>',
+  { noremap = true, silent = true, desc = "Smooth scroll to the top" }
+)
+
+-- Прокрутка в конец файла с динамическим расчетом
+map(
+  "n",
+  "G",
+  ':lua require("neoscroll").scroll(2 * vim.api.nvim_buf_line_count(0), { move_cursor = true, duration = 0.5, easing = quadratic, info = { kind = "G", winid = vim.api.nvim_get_current_win(), bufnr = vim.api.nvim_get_current_buf()} })<CR>',
+  { noremap = true, silent = true, desc = "Smooth scroll to the bottom" }
+)
+
 
 map("n", "gn", function()
   require("nvchad.tabufline").next()
@@ -46,7 +63,7 @@ map("n", "<leader>ld", function()
 end, { desc = "Debug Continue" })
 
 map("n", "<C-t>", function()
-  require("plenary.reload").reload_module("minty.huefy")
+  require("plenary.reload").reload_module "minty.huefy"
   require("minty.huefy").open()
 end, {})
 
@@ -56,7 +73,6 @@ vim.keymap.set("n", "<RightMouse>", function()
   local options = vim.bo.ft == "NvimTree" and "nvimtree" or "default"
   require("menu").open(options, { mouse = true })
 end, {})
-
 
 -- Disable mappings
 nomap("n", "<leader>v") -- drop vertical terminal keys
--- lua/plugins/init.lua
+++ lua/plugins/init.lua
@@ -48,10 +48,46 @@ return {
     end,
   },
 
-
   {
     "nanotee/zoxide.vim",
+    event = "CmdLineEnter",
+  },
+
+  {
+    "karb94/neoscroll.nvim",
     event = "BufEnter",
+    config = function()
+    require('neoscroll').setup {
+      mappings = {                 -- Keys to be mapped to their corresponding default scrolling animation
+        '<C-u>', '<C-d>',
+        '<C-b>', '<C-f>',
+        '<C-y>', '<C-e>',
+        'zt', 'zz', 'zb',
+      },
+      hide_cursor = false,          -- Hide cursor while scrolling
+      stop_eof = true,             -- Stop at <EOF> when scrolling downwards
+      respect_scrolloff = true,   -- Stop scrolling when the cursor reaches the scrolloff margin of the file
+      cursor_scrolls_alone = true, -- The cursor will keep on scrolling even if the window cannot scroll further
+      duration_multiplier = 0.5,   -- Global duration multiplier
+      easing = 'quadratic',           -- Default easing function
+      -- pre_hook = nil,              -- Function to run before the scrolling animation starts
+      post_hook = function(info)
+        if info == nil then
+          return
+        end
+        if info.kind == 'gg' then
+          vim.api.nvim_win_set_cursor(info.winid, { 1, 0 })
+        elseif info.kind == 'G' then
+          local line = vim.api.nvim_buf_line_count(info.bufnr)
+          vim.api.nvim_win_set_cursor(info.winid, { line, 0 })
+        end
+      end,
+      performance_mode = false,    -- Disable "Performance Mode" on all buffers.
+      ignored_events = {           -- Events ignored while scrolling
+          'WinScrolled', 'CursorMoved'
+      },
+    }
+    end,
   },
 
   {
